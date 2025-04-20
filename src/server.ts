07d8ee6fab0137399c274d7738d50cede759cd4c
Vishwajeet-11 <vishwajeetbharadiya12@gmail.com>
2025-04-21 03:57:57 +0530

    final checks

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.get('/', (_req, res) => {
   res.send('API is running 2 ...');
-  console.log("API is running 2")
+  console.log("API is running 2 final checks ✅")
 });
 app.use('/api/users', userRoutes);
 app.use('/api/collections', collectionRoutes);
